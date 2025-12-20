645645e8266a4bc3b7baf8df1a33366828e23b47
HikmetCakir <hikmetcakirdev@outlook.com>
2025-12-20 18:14:14 +0300

    Prevent application crash when kafka is down

--- core-api/src/main/java/com/hikmetcakir/coreapi/config/KafkaProducerConfig.java
+++ core-api/src/main/java/com/hikmetcakir/coreapi/config/KafkaProducerConfig.java
@@ -1,7 +1,6 @@
 package com.hikmetcakir.coreapi.config;
 
-import com.hikmetcakir.coreapi.dto.event.ArticleViewEvent;
-import com.hikmetcakir.coreapi.dto.event.CategoryViewEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@Slf4j
 public class KafkaProducerConfig {
 
     @Bean
@@ -23,11 +23,24 @@ public class KafkaProducerConfig {
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        props.put(ProducerConfig.RETRIES_CONFIG, 0); // Prevent Infinitive Retry
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 5000); // Max Waiting
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 3000);
+        props.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, 3000); // Metadata Waiting Time
+        props.put(ProducerConfig.ACKS_CONFIG, 0); // Don't Wait Even If Not Exist Kafka
         return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
+        KafkaTemplate<String, Object> producer = null;
+        try {
+            producer = new KafkaTemplate<>(producerFactory);
+        }
+        catch (Exception ex) {
+            log.error("Kafka producer can not be started...");
+        }
+        return producer;
     }
 }
