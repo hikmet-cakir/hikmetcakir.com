39c5801471d23b535c1c582d83687a62a89a747d
HikmetCakir <hikmetcakirdev@outlook.com>
2025-12-27 17:57:46 +0300

    Mock KafkaTemplate in article service unit tests

--- core-api/src/test/java/unit/com/hikmetcakir/coreapi/service/ArticleServiceTest.java
+++ core-api/src/test/java/unit/com/hikmetcakir/coreapi/service/ArticleServiceTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.List;
 import java.util.Optional;
@@ -38,6 +39,9 @@ public class ArticleServiceTest {
 
     @Mock
     MongoTemplate mongoTemplate;
+
+    @Mock
+    KafkaTemplate<String, Object> kafkaTemplate;
 
     @Test
     void query_givenArticleTitleMatches_returnOneRecordAsList() {
