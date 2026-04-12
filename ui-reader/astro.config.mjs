4038a6394a394cc2e6da2ae4c85806ff2f332d3f
HikmetCakir <hikmetcakirdev@outlook.com>
2026-04-12 22:10:40 +0300

    Add site indexing structure for google searchings

--- ui-reader/astro.config.mjs
+++ ui-reader/astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://hikmetcakir.com',
   output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
   adapter: node({ mode: 'standalone' }), 
+  integrations: [sitemap()],
   server: {
     port: 4000
   }
