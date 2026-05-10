b67d791a94ee2bbb19786d1c527cd8fa638d5802
HikmetCakir <hikmetcakirdev@outlook.com>
2026-05-10 21:07:56 +0300

    Add compression of uploading image

--- core-api/src/main/java/com/hikmetcakir/coreapi/service/S3Service.java
+++ core-api/src/main/java/com/hikmetcakir/coreapi/service/S3Service.java
@@ -1,6 +1,7 @@
 package com.hikmetcakir.coreapi.service;
 
 import lombok.RequiredArgsConstructor;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,6 +9,7 @@ import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.UUID;
 
@@ -26,13 +28,23 @@ public class S3Service {
     public String uploadFile(MultipartFile file) throws IOException {
         String key = "thumbnails/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
 
+        // Görseli sıkıştır ve yeniden boyutlandır
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Thumbnails.of(file.getInputStream())
+                .width(1280)
+                .outputQuality(0.75)
+                .outputFormat("jpg")
+                .toOutputStream(outputStream);
+
+        byte[] compressedBytes = outputStream.toByteArray();
+
         PutObjectRequest request = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
-                .contentType(file.getContentType())
+                .contentType("image/jpeg")
                 .build();
 
-        s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+        s3Client.putObject(request, RequestBody.fromBytes(compressedBytes));
 
         return "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + key;
     }
